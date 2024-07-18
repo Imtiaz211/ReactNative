b4bfdb720f33d534c8602e66f993f92cd3790f7c
imtiazahmad78692 <imtiazahmad211@gmail.com>
2024-07-18 16:23:59 +0530

    New project init with ReactNative

---
+++ demoReactNative/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+
+  return (
+  <View>
+    <Text style={{fontSize:30}}>Imtiaz Ahmad, React Native</Text>
+    <Text style={{fontSize:30}}>Like</Text>
+    <Text style={{fontSize:30}}>Comment Native</Text>
+    <Text style={{fontSize:30}}>subscribe</Text>
+  </View>    
+  );
+}
+
+export default App;
