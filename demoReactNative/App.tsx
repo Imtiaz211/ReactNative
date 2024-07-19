4d5a5ac3a96486d116c3b6830dff0472ec332b69
imtiazahmad78692 <imtiazahmad211@gmail.com>
2024-07-19 15:55:57 +0530

    Component Creation

--- demoReactNative/App.tsx
+++ demoReactNative/App.tsx
@@ -5,7 +5,7 @@ import {
 } from 'react-native';
 
 const name = "Imtiaz Ahmad";
-let age = 3;
+let age = 36;
 var email = "imtiazahmad211@gmail.com"
 function fruits() {
   return "Imtiaz Apple";
@@ -16,14 +16,16 @@ function App(): React.JSX.Element {
 
   return (
   <View>
-
-
+  <UserData/>
+  { /* Ternery operator */}
   <Text style={{fontSize:30}}>{age === 36 ? "Above 30" : "Unknown age"}</Text>
 
-
+{/* Variables*/}
     <Text style={{fontSize:30}}>{name}</Text>
     <Text style={{fontSize:30}}>{age}</Text>
     <Text style={{fontSize:30}}>{email}</Text>
+
+{/* functions */}
     <Text style={{fontSize:30}}>{fruits()}</Text>
 
     <Text style={{fontSize:30}}>Imtiaz Ahmad, React Native</Text>
@@ -33,5 +35,18 @@ function App(): React.JSX.Element {
   </View>    
   );
 }
+
+{/* Creating components */}
+
+const UserData = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30}}>Name: Imtiaz Ahmad</Text>
+      <Text style={{fontSize:30}}>Age: 36</Text>
+      <Text style={{fontSize:30}}>Email: imtiazahmad211@gmail.com</Text>
+    </View>
+  )
+}
+
 
 export default App;
