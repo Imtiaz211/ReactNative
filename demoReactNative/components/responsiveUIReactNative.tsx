6583b4e2fdd575fe0400c78278bd8664733556e8
imtiazahmad78692 <imtiazahmad211@gmail.com>
2024-07-21 23:04:58 +0530

    code arrange.

--- demoReactNative/components/responsiveUIReactNative.tsx
+++ demoReactNative/components/responsiveUIReactNative.tsx
@@ -1,27 +1,53 @@
-import {View, StyleSheet} from 'react-native'
+import { View, StyleSheet } from 'react-native'
 const ResponsiveUI = () => {
-    return(
-        <View style={{backgroundColor:'red',flex:1}}>
+    return (
+        <View style={Style.main}>
+            <View style={Style.Box1}>
+                <View style={Style.InnerBox1}></View>
+                <View style={Style.InnerBox2}></View>
+                <View style={Style.InnerBox3}></View>
+            </View>
+            <View style={Style.Box2}></View>
+            <View style={Style.Box3}></View>
         </View>
     )
 }
-const Style=StyleSheet.create({
-    main:{
-        backgroundColor:'red',
-        flex:1
+
+
+const Style = StyleSheet.create({
+    main: {
+        flex: 1,
+        backgroundColor: 'red'
     },
-    Box1:{
-        flex:1,
-        backgroundColor:'blue'
+    Box1: {
+        flex: 1,
+        backgroundColor: 'blue',
+        flexDirection: 'row'
     },
-    Box2:{
-        flex:1,
-        backgroundColor:'green'
+    Box2: {
+        flex: 1,
+        backgroundColor: 'green'
     },
-    Box3:{
-        flex:1,
-        backgroundColor:'yellow'
+    Box3: {
+        flex: 1,
+        backgroundColor: 'yellow'
+    },
+    InnerBox1: {
+        flex: 1,
+        backgroundColor: 'skyblue',
+        margin: 10
+    },
+    InnerBox2: {
+        flex: 1,
+        backgroundColor: 'skyblue',
+        margin: 10
+    },
+    InnerBox3: {
+        flex: 1,
+        backgroundColor: 'skyblue',
+        margin: 10
     }
+
 
 })
 // Flex:1 it consume 50%
