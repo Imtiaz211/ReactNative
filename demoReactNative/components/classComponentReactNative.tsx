2e4268b13ce408faa38bb199dc19b2feef8d6e9b
imtiazahmad78692 <imtiazahmad211@gmail.com>
2024-07-21 12:42:11 +0530

    class component with constructor, super()

--- demoReactNative/components/classComponentReactNative.tsx
+++ demoReactNative/components/classComponentReactNative.tsx
@@ -3,15 +3,26 @@ import { Component } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 
 class ClassComponentReactNative extends Component {
+    constructor(props: string) {
+       super(props);
+       this.state={
+        name:'Imtiaz Ahmad'
+       }; 
+    }
+
+    updateName(val: string) {
+        this.setState({name:val})
+    }
     fruit = () => {
         console.warn("Apple");
     }
     render() {
         return (
             <View>
-                <Text style={{ fontSize: 25 }}>Class component in React Native</Text>
+                <Text style={{ fontSize: 25 }}>{this.state.name}</Text>
                 <TextInput
                 placeholder='Enter your name'
+                onChangeText={(text)=>this.updateName(text)}
                 />
                 <Button
                 title='Press me' 
