6f278903c7dde1d0848cfe172e60737143ab8a87
imtiazahmad78692 <imtiazahmad211@gmail.com>
2024-07-23 14:27:45 +0530

    no message

--- demoReactNative/components/npmReactNative.tsx
+++ demoReactNative/components/npmReactNative.tsx
@@ -2,8 +2,7 @@ import { View, Text } from 'react-native'
 import { WebView } from 'react-native-webview'
 const NPMReactNative = () => {
     return (
-        <Text>Learning</Text>
-        // <WebView source={{ uri: "https://reactnative.dev/"}}/>
+        <WebView source={{ uri: "https://reactnative.dev/"}}/>
     )
 }
 export default NPMReactNative
--- demoReactNative/rootFiles/WebviewHome.tsx
+++ demoReactNative/rootFiles/WebviewHome.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { Text } from 'react-native'
-import { WebView } from 'react-native-webview'
 const WebviewHome = () => {
     return (
         
-        <WebView source={{ uri: "https://reactnative.dev/" }} />
     )
 }
 export default WebviewHome
