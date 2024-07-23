ec5e248461abc6717467d82f19147bc95bbaabae
imtiazahmad78692 <imtiazahmad211@gmail.com>
2024-07-23 23:55:03 +0530

    data passing between classes

--- demoReactNative/components/navigationRN/homePage.tsx
+++ demoReactNative/components/navigationRN/homePage.tsx
@@ -1,8 +1,16 @@
 import {View,Text} from 'react-native'
-export const Home = () => {
+export const Home = (props) => {
+    console.warn(props.route.params)
+    const {name, age} =props.route.params
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text style={{ fontSize: 30 }}>Home Screen 1</Text>
+
+            <Text style={{ fontSize: 30 }}>Name:{name}</Text>
+            <Text style={{ fontSize: 30 }}>Age:{age}</Text>
+
+            {/* <Text style={{ fontSize: 30 }}>Name:{props.route.params.name}</Text>
+            <Text style={{ fontSize: 30 }}>Age:{props.route.params.age}</Text> */}
         </View>
     )
 }
--- demoReactNative/components/navigationRN/loginPage.tsx
+++ demoReactNative/components/navigationRN/loginPage.tsx
@@ -1,9 +1,23 @@
-import {View,Text,Button} from 'react-native'
+import { useState } from 'react'
+import {View,Text,Button, TextInput} from 'react-native'
 export const Login = (props) => {
+    const [name, setName]=useState()
+    const age=35
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text style={{ fontSize: 30 }}>Login Screen</Text>
-            <Button title='Go to home page' onPress={() => props.navigation.navigate('Home')} />
+            <TextInput
+            placeholder='Enter your name'
+            style={{fontSize:20, borderColor:'black',borderWidth:2, margin:20}}
+            onChangeText={(text)=>setName(text)}
+            />
+            <Button title='Go to home page' 
+            // Short form to pass data on another class
+            onPress={() => props.navigation.navigate('Home', {name, age})} />
+            {/*  By name pass data on another class */}
+            {/* onPress={() => props.navigation.navigate('Home', {name:name, age:age})} /> */}
+            {/* Static data pass to another class */}
+            {/* onPress={() => props.navigation.navigate('Home', {name:"Imtiaz", age:"36"})} /> */}
         </View>
     )
 
