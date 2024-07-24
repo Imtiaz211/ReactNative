e1cbba7a3918e804ae7c3aae1858ecf9c587e257
imtiazahmad78692 <imtiazahmad211@gmail.com>
2024-07-24 12:28:55 +0530

    dynamic api data parser implemented.

---
+++ demoReactNative/components/apiCallReactNative/listAPICallReactNative.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import { View, Text, ScrollView } from 'react-native'
+export const ListAPICallReactNative = () => {
+
+    const [data, setData] = useState([])
+    const getAPIData = async () => {
+        const url = "https://jsonplaceholder.typicode.com/posts"
+        let result = await fetch(url)
+        result = await result.json();
+        setData(result)
+    }
+    useEffect(() => {
+        getAPIData()
+    })
+    return (
+        <ScrollView>
+            <Text style={{ fontSize: 30 }}>List API Call</Text>
+            {
+                data.length ?
+                    data.map((item) => <View style={{
+                        padding: 10,
+                        borderBottomColor: '#ccc',
+                        borderBottomWidth: 1
+                    }}>
+                        <Text style={{ backgroundColor: '#ddd', textAlign:'center' }}>{item.id}</Text>
+                        <Text style={{fontStyle:'bold', color:'black'}}>{item.title}</Text>
+                        <Text>{item.body}</Text>
+                    </View>
+                    )
+                    :
+                    null
+            }
+        </ScrollView>
+    )
+}
--- demoReactNative/rootFiles/AppH.tsx
+++ demoReactNative/rootFiles/AppH.tsx
@@ -4,11 +4,13 @@ import { View, Text } from 'react-native'
 import NavigationStackReactNative from "../components/navigationRN/navigationReactNative";
 import { TabbarReactNative } from "../components/navigationRN/tabbarReactNative";
 import { APICallReactNative } from "../components/apiCallReactNative/apiCallReactNative";
+import { ListAPICallReactNative } from "../components/apiCallReactNative/listAPICallReactNative";
 
 class AppH extends Component {
     render() {
         return (
-            <APICallReactNative />
+            <ListAPICallReactNative/>
+            // <APICallReactNative />
             // <TabbarReactNative />
             // <NavigationStackReactNative/>
         )
