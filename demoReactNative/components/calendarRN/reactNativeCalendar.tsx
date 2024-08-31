d50e9d6b222d7e19f3f4ff987810a37ce68c5f40
imtiazahmad78692 <imtiazahmad211@gmail.com>
2024-08-31 22:07:28 +0530

    Calendar code added in reactNativeCalendar file.

--- demoReactNative/components/calendarRN/reactNativeCalendar.tsx
+++ demoReactNative/components/calendarRN/reactNativeCalendar.tsx
@@ -1,12 +1,30 @@
-import {View, Text} from "react-native"
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Calendar } from 'react-native-calendars';
 
 
 export const ReactNativeCalendar = () => {
     return (
-    <View>
-        <Text style={{fontSize:30}}>React Native Calendar </Text>
-        <Text style={{fontSize:30}}>Second Line of Calendar</Text>
-    </View>
+        <View style={CalendarStle.container}>
+            <Text style={{ fontSize: 30 }}>React Native Calendar </Text>
+            <Text style={{ fontSize: 30 }}>Second Line of Calendar</Text>
+
+
+            <TouchableOpacity
+                style={{ backgroundColor: 'black', borderRadius: 10, margin: 10 }}>
+                <Text style={{ color: 'white', fontSize: 22 }}>Show Calendar</Text>
+            </TouchableOpacity>
+
+        </View>
 
     )
 }
+
+
+const CalendarStle = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+    }
+})
