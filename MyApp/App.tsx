43fc8f2893d79f0f25881e37b5ee399d7f046116
Imtiaz <imtiaz_ahmad@persistent.com>
2025-10-25 13:50:14 +0530

    folder structure done

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -5,7 +5,7 @@ import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
 
 
 
-const App = () =>{
+const App:React.FC = () =>{
   return (
   <View style={styles.container}>
     <Text style={styles.text}>Imtiaz Ahmad {'\n'} React Native Developers</Text>
