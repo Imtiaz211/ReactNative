5170db659b84954dca066546849cd18b2ec433a4
Imtiaz <imtiaz_ahmad@persistent.com>
2025-10-25 13:59:18 +0530

    style added

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
-
-
-
-
+import appStyle from './src/Utils/TextStyle/textStyle';
 
 const App:React.FC = () =>{
   return (
-  <View style={styles.container}>
-    <Text style={styles.text}>Imtiaz Ahmad {'\n'} React Native Developers</Text>
+  <View style={appStyle.container}>
+    <Text style={appStyle.text}>Imtiaz Ahmad {'\n'} React Native Developers</Text>
     
-    <TextInput style={styles.input}>
+    <TextInput style={appStyle.input}>
       placeholder="Enter your name"
       value= "Imtiaz Ahmad"
     </TextInput>
 
-  <TextInput style={styles.input}>
+  <TextInput style={appStyle.input}>
       placeholder="Enter your password"
       value= "Password"
     </TextInput>
 
     
- <View style={styles.buttonContainer}>
+ <View style={appStyle.buttonContainer}>
         <Button title="Submit" />
       </View>
 
@@ -29,42 +26,5 @@ const App:React.FC = () =>{
   </View>  
   );
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'cyan',
-  },
-  text: {
-    fontSize: 24,
-    color: 'white',
-  },
-
- input: {
-    height: 50,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    paddingHorizontal: 15,
-    fontSize: 16,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3, // for Android shadow
-    marginTop: 20,
-  },
-
-
-  buttonContainer: {
-    marginTop: 25,
-  },
-
-
-});
 
 export default App;
---
+++ MyApp/src/Utils/TextStyle/textStyle.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native'
+const appStyle = StyleSheet.create({
+    container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'cyan',
+  },
+  text: {
+    fontSize: 24,
+    color: 'white',
+  },
+
+ input: {
+    height: 50,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3, // for Android shadow
+    marginTop: 20,
+  },
+
+
+  buttonContainer: {
+    marginTop: 25,
+  },
+});
+export default appStyle;
