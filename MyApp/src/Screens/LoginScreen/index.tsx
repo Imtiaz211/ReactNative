984b264c505ffafc22507e3d526383d21b434a5b
Imtiaz <imtiaz_ahmad@persistent.com>
2025-10-25 14:44:33 +0530

    useEffect added

--- MyApp/src/Screens/LoginScreen/index.tsx
+++ MyApp/src/Screens/LoginScreen/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-import { Text, View, TextInput, Button } from "react-native";
+import { Text, View, TextInput, Alert, TouchableOpacity } from "react-native";
 import appStyle from "../../Utils/TextStyle/textStyle";
-
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen: React.FC = () => {
+
+const [email, setEmail] = React.useState('');
+const [password, setPassword] = React.useState('');
+const navigation = useNavigation();
+
+
+const handleSubmit = () => {
+  if (email === '' && password === '') {
+    Alert.alert('Erro', 'Please enter both email and password');
+    return;
+  }
+  console.log('Logging in with:', email, password);
+//   navigation.navigate('Home');
+}
   return (
     <View style={appStyle.container}>
       <Text style={appStyle.text}>
@@ -12,18 +26,23 @@ const LoginScreen: React.FC = () => {
 
       <TextInput
         style={appStyle.input}
-        placeholder="Enter your name"
-        value="Imtiaz Ahmad"
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
       ></TextInput>
 
       <TextInput
         style={appStyle.input}
-        placeholder="Enter your password"
-        value="Password"
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
       ></TextInput>
 
       <View style={appStyle.buttonContainer}>
-        <Button title="Submit" />
+        <TouchableOpacity>
+            <Text style={appStyle.text}>Sign In</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ReactNative
 React.FC is the type for a functional component.TypeScript ensures type safety and better tooling
+npm install @react-navigation/native // For Navigation
+npm install @react-navigation/stack // For Stack
 
